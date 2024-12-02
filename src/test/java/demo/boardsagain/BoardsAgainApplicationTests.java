50f96c4e1bba8c7977d690da4108edd046202f1b
pgsshiho <s24071@gsm.hs.kr>
2024-12-02 15:31:52 +0900

    asdasd

---
+++ src/test/java/demo/boardsagain/BoardsAgainApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.boardsagain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardsAgainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
